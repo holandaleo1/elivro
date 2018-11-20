76111efdf3c4853f1490c6112738d89e23e8a278
leonardo <leonardoholandanunes@live.com>
2018-11-20 20:20:53 -0300

    deployDebug4

--- djangoEcommerce/settings.py
+++ djangoEcommerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$j$h8xti0v-n91@)xplupju@yfzo1j_mus5)9ibua)3kh*)zwt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =  True
 
 ALLOWED_HOSTS = ['*']
 
